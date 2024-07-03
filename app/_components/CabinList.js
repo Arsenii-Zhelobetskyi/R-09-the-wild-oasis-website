e3362aaac84c2354949dd4a9729eaddc4ba09b84
Arsenii-Zhelobetskyi <arsbrotherplay@gmail.com>
2024-07-03 21:17:55 +0300

    data from cc to sc using URL; data fetching optimisation via nesting

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,7 +1,7 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "@/app/_lib/data-service";
 // import { noStore } from "next/cache";
-async function CabinList() {
+async function CabinList({ filter }) {
   //with noStore is component is nonCached request component
   // noStore() // this request will not be cached, and the page cabins will be regenerated on every request
   // useful with "partial pre-rendering", cause then only the list will be dynamic, and not the cabins page(cabins will stayed static)
@@ -9,9 +9,26 @@ async function CabinList() {
 
   if (!cabins.length) return null;
 
+  let displayedCabins;
+
+  if (filter === "all") {
+    displayedCabins = cabins;
+  }
+  if (filter === "small") {
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  }
+  if (filter === "medium") {
+    displayedCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+    );
+  }
+  if (filter === "large") {
+    displayedCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+  }
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
